25ace8acfb473caff2d4b7568c909138f1d8def1
Martin Pettersson <marpett@kth.se>
2016-10-05 20:52:17 +0200

    pass

---
+++ site.js
@@ -0,0 +1,139 @@
+<div id="app" class="container">
+  <curtain-options></curtain-options>
+</div>
+
+<script type="text/javascript">
+// <![CDATA[
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var hideElement = function(selector) {
+  var doneHiding = false;
+  var observer = new MutationObserver(function(mutations, observer) {
+    if (doneHiding) return;
+    // fired when a mutation occurs
+    var priceElem = document.querySelector(selector);
+    if (priceElem) {
+      priceElem.style.cssText = 'display: none';
+      doneHiding = true;
+    }
+  });
+
+  // define what element should be observed by the observer
+  // and what types of mutations trigger the callback
+  observer.observe(document, {
+    subtree: true,
+    attributes: true
+  });
+
+}
+
+// Price button
+hideElement('div[slot="article-price"');
+
+// Article choices
+hideElement('form[name="articleChoices"]');
+//
+// Article quantity
+hideElement('input[name="quantity"]');
+
+Vue.component('curtain-options', {
+  template: '\
+    <div class="row">\
+      <h4>How many panels?</h4>\
+      <div class="radio">\
+        <label>\
+          <input type="radio" name="panelRadios" value="1" v-model="numPanels">\
+          1\
+        </label>\
+      </div>\
+      <div class="radio">\
+        <label>\
+          <input type="radio" name="panelRadios" value="2" v-model="numPanels">\
+          2\
+        </label>\
+      </div>\
+    </div>\
+    <div class="row">\
+      <div class="col-md-2" v-for="panel in panels" v-if="$index < numPanels">\
+        <h4>Panel {{$index+1}}</h4>\
+        <button class="button" v-on:click="copyRight" v-if="$index == 0">copy right</button>\
+        <button class="button" v-on:click="copyLeft" v-if="$index == 1">copy left</button>\
+        <div class="form-group">\
+          <div class="input-group">\
+            <input v-model="panel.width" type="number" min="1" step="1" class="form-control" placeholder="Width">\
+            <div class="input-group-addon">cm</div>\
+          </div>\
+          <div class="input-group">\
+            <input v-model="panel.height" type="number" min="1" step="1" class="form-control" placeholder="Height">\
+            <div class="input-group-addon">cm</div>\
+          </div>\
+        </div>\
+        <div class="radio">\
+          <label>\
+            <input type="radio" name="mountingRadios{{$index}}" value="wall" v-model="panel.mount">\
+            Wall mounted\
+          </label>\
+        </div>\
+        <div class="radio">\
+          <label>\
+            <input type="radio" name="mountingRadios{{$index}}" value="ceil" v-model="panel.mount">\
+            Ceiling mounted\
+          </label>\
+        </div>\
+        <div class="radio">\
+          <label>\
+            <input type="radio" name="hangingRadios{{$index}}" value="rodpocket" v-model="panel.hanging">\
+            Rod pocket\
+          </label>\
+        </div>\
+        <div class="radio">\
+          <label>\
+            <input type="radio" name="hangingRadios{{$index}}" value="plait2fold" v-model="panel.hanging">\
+            French plait 2-fold\
+          </label>\
+        </div>\
+        <div class="radio">\
+          <label>\
+            <input type="radio" name="hangingRadios{{$index}}" value="plait3fold" v-model="panel.hanging">\
+            French plait 3-fold\
+          </label>\
+        </div>\
+      </div>\
+    </div>\
+  ',
+  data: (function(){ return {
+    numPanels: 1,
+    panels: [
+    {
+      width: 100,
+      height: 240,
+      mount: 'wall',
+      hanging: 'rodpocket',
+      fullness: 1.5
+    },
+    {
+      width: 100,
+      height: 240,
+      mount: 'ceil',
+      hanging: 'plait2fold',
+      fullness: 1.5
+    }]
+  }; }),
+  methods: {
+    copyRight: function() {
+      this.panels[1].width = 200;
+      //this.panels[1] = JSON.parse(JSON.stringify(this.panels[0]));
+    },
+    copyLeft: function() {
+      this.panels[0] = JSON.parse(JSON.stringify(this.panels[1]));
+    }
+  }
+});
+
+new Vue({
+  el: '#app'
+});
+
+// ]]>
+</script>
