1a74b487e9a8fd2adb140035bab76cfa1a00871c
Martin Pettersson <marpett@kth.se>
2016-11-21 20:17:07 +0100

    pass

--- site.js
+++ site.js
@@ -1,8 +1,8 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-var hideElement = function(selector) {
+var hideElement = function (selector) {
   var doneHiding = false;
-  var observer = new MutationObserver(function(mutations, observer) {
+  var observer = new MutationObserver(function (mutations, observer) {
     if (doneHiding) return;
     // fired when a mutation occurs
     var priceElem = document.querySelector(selector);
@@ -18,8 +18,7 @@ var hideElement = function(selector) {
     subtree: true,
     attributes: true
   });
-
-}
+};
 
 // Price button
 hideElement('div[slot="article-price"');
@@ -32,7 +31,7 @@ hideElement('input[name="quantity"]');
 
 var cmToPixles = 1;
 var windowLeft = 190;
-var windowRight = 288+86;
+var windowRight = 288 + 86;
 var rodHeight = 80;
 
 Vue.component('curtain-options', {
@@ -130,39 +129,40 @@ Vue.component('curtain-options', {
       </div>\
     </div>\
   ',
-  data: (function(){ return {
-    numPanels: 2,
-    panel: {
-      width: 100,
-      height: 240,
-      mount: 'wall',
-      hanging: 'rodpocket',
-      fullness: 1.5
-    }
-  }; }),
-  methods: {
+  data: function () {
+    return {
+      numPanels: 2,
+      panel: {
+        width: 100,
+        height: 240,
+        mount: 'wall',
+        hanging: 'rodpocket',
+        fullness: 1.5
+      }
+    };
   },
+  methods: {},
   computed: {
-    pocketSpacing: function() {
+    pocketSpacing: function () {
       var preferred = 40 / this.panel.fullness;
       return this.panel.width / Math.round(this.panel.width / preferred);
     },
     price: function () {
-      var widthMeters = (this.panel.width*this.panel.fullness)/100;
-      var heightMeters = this.panel.height/100;
+      var widthMeters = this.panel.width * this.panel.fullness / 100;
+      var heightMeters = this.panel.height / 100;
       var meterPrice = 300;
       return Math.round(widthMeters * heightMeters * meterPrice);
     },
-    panelFolds: function() {
+    panelFolds: function () {
       var folds = [];
       for (var i = 0; i < this.numPanels; i++) {
         var rect = this.panelRects[i];
         console.log(this.pocketSpacing);
-        var halfSpacing = this.pocketSpacing/2;
-        for (var x = rect[0]+halfSpacing; x < rect[0]+rect[2]; x += halfSpacing) {
+        var halfSpacing = this.pocketSpacing / 2;
+        for (var x = rect[0] + halfSpacing; x < rect[0] + rect[2]; x += halfSpacing) {
           console.log(x);
-          var fold ={ x: x, y: rodHeight, height: rect[3] };
-          var str = "M"+fold.x.toString()+","+fold.y.toString()+" c0,0,0,0,0,"+fold.height.toString();
+          var fold = { x: x, y: rodHeight, height: rect[3] };
+          var str = "M" + fold.x.toString() + "," + fold.y.toString() + " c0,0,0,0,0," + fold.height.toString();
           console.log(str);
           folds.push(str);
         }
@@ -170,49 +170,32 @@ Vue.component('curtain-options', {
 
       return folds;
     },
-    panelRects: function() {
-      return [
-        [
-          windowLeft-this.panel.width*cmToPixles,
-          rodHeight,
-          this.panel.width*cmToPixles,
-          this.panel.height*cmToPixles
-        ],
-        [
-          windowRight,
-          rodHeight,
-          this.panel.width*cmToPixles,
-          this.panel.height*cmToPixles
-        ]
-      ]
+    panelRects: function () {
+      return [[windowLeft - this.panel.width * cmToPixles, rodHeight, this.panel.width * cmToPixles, this.panel.height * cmToPixles], [windowRight, rodHeight, this.panel.width * cmToPixles, this.panel.height * cmToPixles]];
     },
-    panelPaths: function() {
-      strings = []
+    panelPaths: function () {
+      strings = [];
       for (var i = 0; i < this.numPanels; i++) {
         var rect = this.panelRects[i];
-        var str = "M"+rect[0].toString()+","+rect[1].toString()+" c0,0,0,0,"+
-                  (rect[2]).toString()+","+(0).toString()+" c0,0,0,0," +
-                  (0).toString()+","+(rect[3]).toString()+" c0,0,0,0,"+
-                  (-rect[2]).toString()+","+(0).toString()+" c0,0,0,0,"+
-                  (0).toString()+","+(-rect[3]).toString();
+        var str = "M" + rect[0].toString() + "," + rect[1].toString() + " c0,0,0,0," + rect[2].toString() + "," + 0 .toString() + " c0,0,0,0," + 0 .toString() + "," + rect[3].toString() + " c0,0,0,0," + (-rect[2]).toString() + "," + 0 .toString() + " c0,0,0,0," + 0 .toString() + "," + (-rect[3]).toString();
 
         var numFolds = Math.floor(this.panel.width / this.pocketSpacing);
-        var str = "M"+rect[0].toString()+","+rect[1].toString();
+        var str = "M" + rect[0].toString() + "," + rect[1].toString();
         for (var x = 0; x < numFolds; x++) {
           var f = (this.pocketSpacing / 2).toString();
-          str += "c"+f+","+f+","+f+",-"+f+","+(this.pocketSpacing).toString()+",0 ";
-          console.log(str);
-        }
-          
-        str += "c0,0,0,0," + (0).toString()+","+(rect[3]).toString()+" ";
-
-        for (var x = 0; x < numFolds; x++) {
-          var f = (this.pocketSpacing / 2).toString();
-          str += "c-"+f+","+f+",-"+f+",-"+f+",-"+(this.pocketSpacing).toString()+",0 ";
+          str += "c" + f + "," + f + "," + f + ",-" + f + "," + this.pocketSpacing.toString() + ",0 ";
           console.log(str);
         }
 
-        str += "c0,0,0,0," + (0).toString()+","+(-rect[3]).toString();
+        str += "c0,0,0,0," + 0 .toString() + "," + rect[3].toString() + " ";
+
+        for (var x = 0; x < numFolds; x++) {
+          var f = (this.pocketSpacing / 2).toString();
+          str += "c-" + f + "," + f + ",-" + f + ",-" + f + ",-" + this.pocketSpacing.toString() + ",0 ";
+          console.log(str);
+        }
+
+        str += "c0,0,0,0," + 0 .toString() + "," + (-rect[3]).toString();
 
         //var str = "M"+((i+1)*100)+",100 c20,20-20,-20,100,0";
         strings.push(str);
